c4db2a56d695326186eeda6d0ad9ee74bf5fe662
Madelaine .S <madelaine.starke21@my.stjohns.edu>
2023-10-02 23:03:04 -0400

    Changes added

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,37 +1,36 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
-public class UniqueWords
-{
+public class UniqueWords {
    /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
+    * Counts the number of unique strings in a list.
+    *
+    * @param list ArrayList of strings to be examined.
+    * @return number of unique strings in the list.
+    */
+   public static int countUnique(ArrayList<String> list) {
+      HashSet<String> uniqueWords = new HashSet<>();
+
+      // Iterate through the ArrayList and add each unique word to the HashSet.
+      for (String word : list) {
+         uniqueWords.add(word);
       }
-	  return count;
+
+      // The size of the HashSet is the count of unique words.
+      return uniqueWords.size();
    }
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
+   public static void main(String[] args) {
+      ArrayList<String> words = new ArrayList<>();
       words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
+      words.add("orange");
+      words.add("blackboard");
+      words.add("apple");
+      words.add("orange");
+      words.add("sun");
+      words.add("moon");
+
+      int unique = countUnique(words);
       System.out.println(words + " has " + unique + " unique words");
    }
 }
