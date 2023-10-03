7a6d519650e9afc0cf8ab9304be44cd4d9d46763
Madelaine .S <madelaine.starke21@my.stjohns.edu>
2023-10-02 23:09:36 -0400

    Changes added

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 
+/**
+ * This class provides a utility for counting the number of unique strings in a list.
+ */
 public class UniqueWords {
    /**
     * Counts the number of unique strings in a list.
     *
     * @param list ArrayList of strings to be examined.
-    * @return number of unique strings in the list.
+    * @return The number of unique strings in the list.
     */
    public static int countUnique(ArrayList<String> list) {
       HashSet<String> uniqueWords = new HashSet<>();
@@ -20,6 +23,13 @@ public class UniqueWords {
       return uniqueWords.size();
    }
 
+   /**
+    * This is the main method of the UniqueWords class.
+    * It takes an array of strings as input and does something.
+    * You should describe what this method does in detail.
+    *
+    * @param args The command-line arguments.
+    */
    public static void main(String[] args) {
       ArrayList<String> words = new ArrayList<>();
       words.add("apple");
